6407dbb9ffaf4489c06521d0f77da74733164f10
sydasif <sydasif78@gmail.com>
2022-06-19 16:09:19 +0500

    add variable exe

---
+++ ex_02_variable.py
@@ -0,0 +1,19 @@
+# Python variable
+print("There was man in a city name, Mike.")
+print("His age is 35 yr, old")
+print("He is a good man but do not like his name, Mike.")
+print("He was happy with his age.")
+
+# Add variable
+man_name = "John"
+man_age = "28"             #string
+print("There was man in a city name "+ man_name + ".")
+print("He is "+ man_age + " year, old")
+print("He is a good man but do not like his name "+ man_name + ".")
+print("He was happy with his "+ man_age + ".")
+
+# Change variable value
+man_name = "Mike"           #string
+man_age = 30                #integer
+print("He is a good man but do not like his name "+ man_name + ".")
+print("He was happy with his " + str(man_age) + ".")
