2db75e7dc5a57fc11a13b7653b5fb1cf011f1692
sydasif <sydasif78@gmail.com>
2022-06-21 18:29:06 +0500

    add exe 04 number in python

---
+++ ex_04_number.py
@@ -0,0 +1,29 @@
+# numbers are data type in python
+
+print(2) # whole number is integer in python
+print(2.0987) # decimal number is float
+print(-2.098) # negative (float)
+print(3 + 4) # addition
+print(3 + 4.5) # float addition
+print(3 - 4) # subtraction
+print(3 / 4) # divide
+print(3 * 4) # multiply
+# order of operation
+print(3 * 4 + 5)  # ans=17
+print(3 * (4 + 5)) # ans=27
+print(10 % 3) # 3 divide by 3 = reminder 1 
+
+# numbers also store in variables
+my_num = 5
+print(my_num)
+# convert number into string
+print(str(my_num))
+# number concatenation into string 
+print(str(my_num) + " my favorite number")
+
+# function
+print(min(4, 6)) # print min number
+# print max number
+print(max(4 , 6))
+# print decimal number in round
+print(round(2.3))
