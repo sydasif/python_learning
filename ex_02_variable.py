2a773e361b1e6d1c6ef01da9b6a76fd7e580a79d
sydasif <sydasif78@gmail.com>
2022-06-22 08:59:33 +0500

    amend ex_02

--- ex_02_variable.py
+++ ex_02_variable.py
@@ -8,7 +8,7 @@ print("He was happy with his age.")
 man_name = "John"
 man_age = "28"             #string
 print("There was man in a city name "+ man_name + ".")
-print("He is "+ man_age + " year, old")
+print("His age is "+ man_age + " year, old")
 print("He is a good man but do not like his name "+ man_name + ".")
 print("He was happy with his "+ man_age + ".")
 
