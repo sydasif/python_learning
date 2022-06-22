340f9156d49a5d4637ab616840e8857716b65bc8
sydasif <sydasif78@gmail.com>
2022-06-22 09:20:47 +0500

    add user input exe

---
+++ ex_05_user_input.py
@@ -0,0 +1,11 @@
+# input from user to store in variable
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello, " + name + "! your age is " + age + ".")
+
+# 2nd Method
+print("Enter your name: ")
+name = input('>>> ')
+print("Enter your age: ")
+age = input('>>> ')
+print("Hello, " + name + "! your age is " + age + ".")
