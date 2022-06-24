35a8e3962551fc3962f7f9d47cdb7908c2236c11
sydasif <sydasif78@gmail.com>
2022-06-24 15:59:58 +0500

    add cal

---
+++ exe_01_calculator.py
@@ -0,0 +1,17 @@
+# it will concatenate two string
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+print(result) 
+
+"""it will give value error"""
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = int(num1) + int(num2)
+#print(result)
+
+# it will add both float and integer
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
